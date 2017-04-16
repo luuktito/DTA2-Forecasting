3edcf7a3a9bc8ac72e91b6a8295cf4c9cc3d9f31
Luuk <luuktito@gmail.com>
2017-04-16 18:03:21 +0200

    Remove unused namespaces

--- Forecasting/Form1.cs
+++ Forecasting/Form1.cs
@@ -1,16 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Runtime.InteropServices;
-using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Forecasting
 {
