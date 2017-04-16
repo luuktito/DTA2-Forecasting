2984bcae25fb392cd28abb70922dd94e1bb73e4b
Luuk <Luuk@DESKTOP-TSB3G0E>
2017-04-16 17:47:18 +0200

    Add project files.

---
+++ Forecasting/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Forecasting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series13 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series14 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series15 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title5 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.Chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Chart1
+            // 
+            chartArea5.Area3DStyle.Inclination = 10;
+            chartArea5.Area3DStyle.LightStyle = System.Windows.Forms.DataVisualization.Charting.LightStyle.Realistic;
+            chartArea5.Area3DStyle.PointDepth = 50;
+            chartArea5.Area3DStyle.PointGapDepth = 10;
+            chartArea5.Area3DStyle.Rotation = 10;
+            chartArea5.AxisX.Title = "Month";
+            chartArea5.AxisX.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            chartArea5.AxisY.Title = "Demand (units)";
+            chartArea5.AxisY.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            chartArea5.CursorX.IsUserEnabled = true;
+            chartArea5.CursorX.IsUserSelectionEnabled = true;
+            chartArea5.CursorY.IsUserEnabled = true;
+            chartArea5.CursorY.IsUserSelectionEnabled = true;
+            chartArea5.Name = "ChartArea1";
+            this.Chart1.ChartAreas.Add(chartArea5);
+            legend5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            legend5.IsTextAutoFit = false;
+            legend5.Name = "Legend1";
+            this.Chart1.Legends.Add(legend5);
+            this.Chart1.Location = new System.Drawing.Point(212, 23);
+            this.Chart1.Name = "Chart1";
+            series13.ChartArea = "ChartArea1";
+            series13.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series13.LabelToolTip = "#VALX";
+            series13.Legend = "Legend1";
+            series13.MarkerSize = 6;
+            series13.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series13.Name = "Data";
+            series14.ChartArea = "ChartArea1";
+            series14.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series14.Legend = "Legend1";
+            series14.MarkerSize = 6;
+            series14.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series14.Name = "SES";
+            series15.ChartArea = "ChartArea1";
+            series15.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series15.Legend = "Legend1";
+            series15.MarkerSize = 6;
+            series15.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series15.Name = "DES";
+            this.Chart1.Series.Add(series13);
+            this.Chart1.Series.Add(series14);
+            this.Chart1.Series.Add(series15);
+            this.Chart1.Size = new System.Drawing.Size(754, 540);
+            this.Chart1.TabIndex = 0;
+            this.Chart1.Text = "chart1";
+            title5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            title5.Name = "Title1";
+            title5.Text = "Forecasting (SES, DES) for sword demand per month";
+            this.Chart1.Titles.Add(title5);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(13, 23);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(176, 540);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1001, 592);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart Chart1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Forecasting/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Forecasting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Read SwordsDemand.csv containing 36 rows
+            var dataSetSize = File.ReadAllLines("SwordsDemand.csv").Count();
+            var dataSet = File.ReadAllLines("SwordsDemand.csv").Select(
+                    line => double.Parse(line.Split(',').ElementAt(1)))
+                .ToList();
+
+            for (var i = 0; i < dataSet.Count; i++)
+            {
+                Chart1.Series["Data"].Points.AddXY(i + 1, dataSet[i]);
+            }
+
+            var dataSetSES = CalculateSES(dataSet, 12);
+            for (var i = 0; i < dataSetSES.Item2.Count; i++)
+            {
+                Chart1.Series["SES"].Points.AddXY(i + 1, dataSetSES.Item2[i]);
+            }
+
+            var dataSetDES = CalculateDES(dataSet, 12);
+            for (var i = 0; i < dataSetDES.Item2.Count; i++)
+            {
+                Chart1.Series["DES"].Points.AddXY(i + 2, dataSetDES.Item2[i]);
+            }
+
+            textBox1.AppendText("Best Alpha for SES: " + dataSetSES.Item1.Item1 + Environment.NewLine);
+            textBox1.AppendText("Best Error for SES: " + dataSetSES.Item1.Item2 + Environment.NewLine + Environment.NewLine);
+            textBox1.AppendText("Best Alpha for DES: " + dataSetDES.Item1.Item1 + Environment.NewLine);
+            textBox1.AppendText("Best Beta for DES: " + dataSetDES.Item1.Item2 + Environment.NewLine);
+            textBox1.AppendText("Best Alpha for DES: " + dataSetDES.Item1.Item3 + Environment.NewLine);
+        }
+
+        private Tuple<Tuple<double, double>, List<double>> CalculateSES(List<double> dataSet, int forecastAmount)
+        {
+            //var alphaSESFinal = 0.0;
+            var alphaErrorSES = new List<Tuple<double, double>>();
+
+            for (var i = 0.0; i < 1.0; i += 0.1) 
+            {
+                var alphaSES = i;
+                var SSE = 0.0;
+                var dataSetSES = new List<double> { dataSet[0] };
+                
+                for (var j = 1; j < dataSet.Count; j++)
+                {
+                    var smoothedPointSES = (alphaSES * dataSet[j - 1]) + (1 - alphaSES) * (dataSetSES[j - 1]);
+                    dataSetSES.Add(smoothedPointSES);
+                    SSE += Math.Pow((smoothedPointSES - dataSet[j]), 2);
+                }
+                SSE = Math.Sqrt(SSE / (dataSet.Count - 1));
+                alphaErrorSES.Add(new Tuple<double, double>(i, SSE));
+            }
+
+            var alphaErrorSESFinal = alphaErrorSES.Aggregate((l, r) => (l.Item2 < r.Item2) ? l : r);
+            var alphaSESFinal = alphaErrorSESFinal.Item1;
+
+            var dataSetSESFinal = new List<double> { dataSet[0] };
+            for (var i = 1; i < dataSet.Count + forecastAmount; i++)
+            {
+                var smoothedPointSES = 0.0;
+                if (i >= dataSet.Count) 
+                    smoothedPointSES = dataSetSESFinal[dataSet.Count - 1];
+                else
+                    smoothedPointSES = (alphaSESFinal * dataSet[i]) + (1 - alphaSESFinal) * (dataSetSESFinal[i - 1]);
+                dataSetSESFinal.Add(smoothedPointSES);
+            }
+            var resultsSES = new Tuple<Tuple<double, double>, List<double>>(alphaErrorSESFinal, dataSetSESFinal);
+            return resultsSES;
+        }
+
+        private Tuple<Tuple<double, double, double>, List<double>> CalculateDES(List<double> dataSet, int forecastAmount)
+        {
+            var alphaBetaErrorDES = new List<Tuple<double, double, double>>();
+
+            for (var i = 0.0; i < 1.0; i += 0.1)
+            {
+                var alphaDES = i;
+
+                for (var j = 0.0; j < 1.0; j += 0.1) 
+                {
+                    var betaDES = j;
+                    var dataSetDES = new List<double> { dataSet[0], dataSet[1] };
+                    var dataSetTrendDES = new List<double> { 0, dataSet[1] - dataSet[0] };
+                    var dataSetForecastDES = new List<double>();
+                    var SSE = 0.0;
+                    for (var k = 2; k < dataSet.Count; k++)
+                    {
+                        var smoothedPointDES = (alphaDES * dataSet[k]) + (1 - alphaDES) * (dataSetDES[k - 1] + dataSetTrendDES[k - 1]);
+                        dataSetDES.Add(smoothedPointDES);
+                        var trendPointDES = (betaDES * (dataSetDES[k] - dataSetDES[k - 1])) + (1 - betaDES) * dataSetTrendDES[k - 1];
+                        dataSetTrendDES.Add(trendPointDES);
+                        var forecastPointDES = dataSetDES[k-1] + dataSetTrendDES[k-1];
+                        dataSetForecastDES.Add(forecastPointDES);
+                        SSE += Math.Pow((forecastPointDES - dataSet[k]), 2);
+                    }
+                    SSE = Math.Sqrt(SSE / (dataSet.Count - 2));
+                    alphaBetaErrorDES.Add(new Tuple<double, double, double>(i, j, SSE));
+                }
+            }
+            foreach (var tuple in alphaBetaErrorDES) {
+                Console.WriteLine(tuple);
+            }
+            var alphaErrorDESFinal = alphaBetaErrorDES.Aggregate((l, r) => (l.Item3 < r.Item3) ? l : r);
+            var alphaDESFinal = alphaErrorDESFinal.Item1;
+            var betaDESFinal = alphaErrorDESFinal.Item2;
+
+            var dataSetDESFinal = new List<double> { dataSet[0], dataSet[1] };
+            var dataSetTrendDESFinal = new List<double> { 0, dataSet[1] - dataSet[0] };
+            var dataSetForecastDESFinal = new List<double>();
+
+            for (var i = 2; i < dataSet.Count + forecastAmount; i++)
+            {
+                var smoothedPointDES = 0.0;
+                var trendPointDES = 0.0;
+                var forecastPointDES = 0.0;
+
+                if (i >= dataSet.Count)
+                {
+                    forecastPointDES = dataSetDESFinal[dataSet.Count - 1] + (dataSetTrendDESFinal[dataSet.Count - 1] * (i - dataSet.Count));
+                    dataSetForecastDESFinal.Add(forecastPointDES);
+                }
+                else 
+                {
+                    smoothedPointDES = (alphaDESFinal * dataSet[i]) + (1 - alphaDESFinal) * (dataSetDESFinal[i - 1] + dataSetTrendDESFinal[i - 1]);
+                    dataSetDESFinal.Add(smoothedPointDES);
+                    trendPointDES = (betaDESFinal * (dataSetDESFinal[i] - dataSetDESFinal[i - 1])) + (1 - betaDESFinal) * dataSetTrendDESFinal[i - 1];
+                    dataSetTrendDESFinal.Add(trendPointDES);
+                    forecastPointDES = dataSetDESFinal[i - 1] + dataSetTrendDESFinal[i - 1];
+                    dataSetForecastDESFinal.Add(forecastPointDES);
+                }
+    
+            }
+            var resultsDES = new Tuple<Tuple<double, double, double>, List<double>>(alphaErrorDESFinal, dataSetForecastDESFinal);
+            return resultsDES;
+        }
+    }
+
+}
